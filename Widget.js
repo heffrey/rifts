d89e4977375f6ea910dca5f086c26e910651356b
Jeff Whiteside <jeffrey.whiteside@gmail.com>
2019-05-15 21:12:47 -0700

    Update Widget.js

--- Widget.js
+++ Widget.js
@@ -1,7 +1,6 @@
-// A widget is a static html rendered using a javascript object. It may follow by an input for those options once selected
+// A widget is a static html rendered using a javascript object. It may be followed by an input for options once selected.
 // The selected item will call event handlers and other actions. By default, it is invoked using a class extending WidgetObject
-// which ensures the presence of a options that are handled and rendered dynamically. The icon that is clicked is referred 
-// to as an icon. The resulting action populates a reliedOn Object, with precomputed data present.
+// which ensures the presence of an options member. Resulting actions populate a reliedOn Object.
 
 class Widget {
   
